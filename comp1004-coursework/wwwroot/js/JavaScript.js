974347bf9c638e46ed13273afd1062393f3b164c
Tyler-Shell <tyler.shell@students.plymouth.ac.uk>
2025-04-03 04:56:09 +0100

    com

---
+++ comp1004-coursework/wwwroot/js/JavaScript.js
@@ -0,0 +1,15 @@
+﻿
+$(document).ready(function () {
+
+    $('index').click(function () {
+        $('#page-content-wrapper').load('index.html');
+    })
+
+    $('#game').click(function () {
+        $('#page-content-wrapper').load('game.html');
+    })
+
+    $('#data').click(function () {
+        $('#page-content-wrapper').load('data.html');
+    })
+});
