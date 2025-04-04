947ef6706ffabb4c6a338c80153b37776c9ac203
Tyler-Shell <tyler.shell@students.plymouth.ac.uk>
2025-04-04 02:04:38 +0100

    ghj

---
+++ comp1004-coursework/wwwroot/js/datascript.js
@@ -0,0 +1,11 @@
+﻿var username = ''
+
+function storeInputValue() {
+    const inputElement = document.getElementById('Username');
+    const inputValue = inputElement.value;
+    username = inputValue;
+}
+
+function returnUsername() {
+    return username;
+}
